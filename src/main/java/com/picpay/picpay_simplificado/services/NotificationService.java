90a7f319d9fc427f7f38a45e74ac7941099ea8d3
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-31 00:12:35 -0300

    fix: API service was not found

--- src/main/java/com/picpay/picpay_simplificado/services/NotificationService.java
+++ src/main/java/com/picpay/picpay_simplificado/services/NotificationService.java
@@ -18,11 +18,15 @@ public class NotificationService {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
+        /*
         ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
 
         if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
             System.out.println("Erro ao enviar notificação");
             throw new Exception("Serviço de notificação está fora do ar.");
         }
+
+         */
+        System.out.println("Notificação enviada para o usuário.");
     }
 }
