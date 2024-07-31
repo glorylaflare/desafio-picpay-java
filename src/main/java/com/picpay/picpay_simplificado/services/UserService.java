01c59c82ea23f3d10974762b66256ea57ec5c071
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-31 00:55:29 -0300

    feat: modifying validation responsibility

--- src/main/java/com/picpay/picpay_simplificado/services/UserService.java
+++ src/main/java/com/picpay/picpay_simplificado/services/UserService.java
@@ -17,16 +17,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void validadeTransaction(User sender, BigDecimal amount) throws Exception {
-        if(sender.getUserType() == UserType.MERCHANT) {
-            throw new Exception("Usuário do tipo logista não está autorizado a realizar transação.");
-        }
-
-        if(sender.getBalance().compareTo(amount) < 0) {
-            throw new Exception("Saldo insuficiente.");
-        }
-    }
-
     public User findUserById(Long id) throws Exception {
         return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
     }
