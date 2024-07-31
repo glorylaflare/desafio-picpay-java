5f9c97ebe6be029595ec77c947399ed8925da3ac
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-30 21:25:16 -0300

    fix: adjusting the Transaction import

--- src/main/java/com/picpay/picpay_simplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpay/picpay_simplificado/repositories/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.picpay.picpay_simplificado.repositories;
 
-import jakarta.transaction.Transaction;
+import com.picpay.picpay_simplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
