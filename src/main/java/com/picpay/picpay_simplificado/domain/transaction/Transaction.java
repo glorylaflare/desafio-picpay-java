64df214930b6d0b8749ecfdb2d6800403603fec8
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-30 21:11:24 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpay/picpay_simplificado/domain/transaction/Transaction.java
@@ -0,0 +1,30 @@
+package com.picpay.picpay_simplificado.domain.transaction;
+
+import com.picpay.picpay_simplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Transactions")
+@Table(name = "Transactions")
+@Getter @Setter @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
