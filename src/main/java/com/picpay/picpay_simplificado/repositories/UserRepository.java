9f54f5803f7f927aba9b10a652d092d723e07950
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-30 21:11:36 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpay/picpay_simplificado/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpay.picpay_simplificado.repositories;
+
+import com.picpay.picpay_simplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
