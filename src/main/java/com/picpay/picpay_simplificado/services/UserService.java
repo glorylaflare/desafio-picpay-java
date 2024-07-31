c64716df655d2436ae469e8ec7c42746128e160a
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-30 22:26:01 -0300

    feat: add createUser method

--- src/main/java/com/picpay/picpay_simplificado/services/UserService.java
+++ src/main/java/com/picpay/picpay_simplificado/services/UserService.java
@@ -2,11 +2,14 @@ package com.picpay.picpay_simplificado.services;
 
 import com.picpay.picpay_simplificado.domain.user.User;
 import com.picpay.picpay_simplificado.domain.user.UserType;
+import com.picpay.picpay_simplificado.dtos.UserDTO;
 import com.picpay.picpay_simplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -30,5 +33,16 @@ public class UserService {
 
     public void saveUser(User user) {
         this.userRepository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+
+        return  newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 }
